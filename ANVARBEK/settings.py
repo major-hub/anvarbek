460436487425d25f26e96eed6af4f1e009bcda7d
major-hub <mrsol1jonovi4@gmail.com>
2022-02-22 12:11:15 +0500

    added STATICFILES_DIRS

--- ANVARBEK/settings.py
+++ ANVARBEK/settings.py
@@ -128,6 +128,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = [
+    BASE_DIR / 'anvarbek_app/static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
